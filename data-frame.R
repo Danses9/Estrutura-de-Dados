d8308599f02eda91d4ef6bfd6df8248a7efeb9a4
Danna <Danna.ses9@gmail.com>
2023-06-25 14:35:38 -0300

    Aprendendo a manipular dados e utilizando funções no data frame

--- data-frame.R
+++ data-frame.R
@@ -44,12 +44,63 @@ dt.f2$Elementos
 # a mesma saída é retornada na forma de um vetor de elemento. 
 
 
-# Para adicionar linhas usamos a função rbind()
+# Para adicionar novas linhas usamos a função rbind()
 
 nova_linha <- rbind(dt.f, c("Força", 110, 180))
 nova_linha
 
-# Para adicionar colunas use a função cbind()
+# Para adicionar novas colunas use a função cbind()
 
 nova_linha2 <- cbind(nova_linha, Passos = c(6000, 2000, 8000, 400))
 nova_linha2
+
+# Para remover linhas e colunas em um Data Frame utilize c()
+
+dt.f.new <- dt.f[-c(1), -c(1)]
+dt.f.new
+
+
+# Para saber a quantidade de linhas e colunas utilize a função dim()
+
+dim(dt.f.new)
+dim(dt.f2)
+
+# Podemos usar a função ncol() para encontrar o número de colunas e nrow() para o número de linhas
+
+ncol(dt.f.new)
+ncol(dt.f2)
+nrow(dt.f.new)
+nrow(dt.f2)
+
+# Use a função length() para encontrar o número de colunas em um quadro de dados (semelhante à ncol())
+
+length(dt.f.new)
+length(dt.f2)
+
+# Combinando o quadro de dados
+
+A <- data.frame(
+  Balas            = c("Tic-Tac", "7 Belo", "Halls", "Bala de Goma"),
+  Doce_com_Frutas  = c("Maçã do Amor", "Bananinha", "Doce de Ameixa", "Abacaxi em calda"),
+  Legumes          = c("Chuchu", "Beterraba", "Mandioca", "Abobora"),
+  Doces            = c("Chocolate", "Bombom de Cereja", "M&M", "Kit-Kat"),
+  Sucos            = c("Abacaxi", "Laranja", "Pessêgo", "Maracujá")  
+)
+
+A
+
+B <- data.frame(
+  Balas           = c("Mentos", "Yorgute 100", "Lua Cheia", "Dadinho"),
+  Doce_com_Frutas = c("Doce de Mamão", "Salada de Frutas", "Bolo de Banana", "Merengue de Morango"),
+  Legumes         = c("Batata", "Berinjela", "Couve", "Inhame"),
+  Doces           = c("Paçoca", "Doce de Leite", "Musse", "Pudim"),
+  Sucos           = c("Acerola", "Morango", "Limão", "Açai")  
+)
+
+B
+
+tabela.final <- rbind(A, B)
+tabela.final
+
+tabela.final <- cbind(A, B)
+tabela.final
