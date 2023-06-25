6e59e8c8f73d51cceb9a55a3c72f96eefafdda4a
Danna <Danna.ses9@gmail.com>
2023-06-25 14:37:25 -0300

    Criando uma tabela como lista de mercado

---
+++ aplicando-data.frame.R
@@ -0,0 +1,10 @@
+# Como será a sua lista de mercado? 
+
+Mercado <- data.frame(
+           Higiene      = c("Óleo corporal", "Desodorante", "Gillete"),
+           Proteinas    = c("Ovos", "Peixe", "Carne"),
+           Besteirinhas = c("Danone", "Bolacha de água e sal", "Bolacha recheada"),
+           Aleatórios   = c("Toneira para lavanderia", "Coisas Zero Lactose", "Gelatina")
+)
+
+Mercado
